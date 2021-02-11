4d925b2671a2dac8d8e9ef71a43b1822dbf0aca3
Kevin Giovanni <kevinevraldo@gmail.com>
2021-02-11 21:32:36 +0700

    init project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,68 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="(rate, key) in rates" :key="key">
+        {{ `${key} = ${rate}` }}
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <p>Base Currency</p>
+    <select v-model="baseCurrency">
+      <option v-for="(rate, key) in rates" :key="key" :value="key">{{ key }}</option>
+    </select>
+
+    <p>Target Currency</p>
+    <select v-model="targetCurrency">
+      <option v-for="(rate, key) in rates" :key="key" :value="key">{{ key }}</option>
+    </select>
+
+    <p>Input Amount</p>
+    <input type="text" v-model="amount">
+    <p>Result</p>
+    <strong>{{ targetCurrency }} {{ convertedAmount }}</strong>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String,
+  },
+  data: () => ({
+    rates: [],
+    amount: 1,
+    targetCurrency: null,
+    baseCurrency: null,
+  }),
+  methods: {
+    async fetchData(currency = 'USD') {
+      const url = `https://api.openrates.io/latest?base=${currency}`;
+      // destruct the object
+      const {
+        data: {
+          rates,
+        },
+      } = await axios.get(url);
+      this.rates = rates;
+    },
+  },
+  watch: {
+    baseCurrency(val) {
+      this.fetchData(val);
+    },
+  },
+  computed: {
+    convertedAmount() {
+      // ternary operation
+      return this.targetCurrency ? this.rates[this.targetCurrency] * this.amount : 0;
+    },
+  },
+  mounted() {
+    this.fetchData();
   },
 };
 </script>
